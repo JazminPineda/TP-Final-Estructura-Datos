a842506889fb7c28d57b8bf289024c74cb3a2db2
Jazmin <jppinedac940@hotmail.com>
2020-11-18 20:41:50 -0300

    se agrega funcionalidades tp final v1

---
+++ TrabajoFinal2Estructura_Datos/Principal.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TrabajoFinal2Estructura_Datos
+{
+    class Principal
+    {
+        public Queue<int> miCola; //atributo comparte en diferentes funciones en diferentes clases; se le agrega el tipo para que traiga la func revers
+
+        public void Crear_Cola()
+        {
+            miCola = new Queue<int>(); // cración de Cola en atributo
+        }
+
+
+        public void Borrar_Cola()
+        {
+            miCola.Clear();
+        }
+
+        public void Agregar_Pedido()
+        {
+            if (miCola == null) 
+            {
+                Console.WriteLine("No hay cola, favor crear cola opción 1 ");
+            }
+            
+            else
+            {
+                Console.WriteLine("Ingrese pedido ");
+                string pedido = Console.ReadLine();
+                int pedidos = 0;
+
+
+                bool EsNumero = int.TryParse(pedido, out pedidos); //out salida del entero, resultado se guarada booleano para usar en validaciones V o F resultado
+                while (!EsNumero)// cuando no se puede convertir en int queda en F, tenemos la negacion conviert el F o V y entra al while
+
+                {
+                    Console.WriteLine("\n\nEl pedido no es un número");
+                    Console.WriteLine("\n\nIngrese nuevamente el pedido");
+                    pedido = Console.ReadLine();
+                    EsNumero = int.TryParse(pedido, out pedidos);
+                }
+                //cuando lo puede convertir a numero es falso
+
+                miCola.Enqueue(pedidos);
+
+
+            }
+            
+
+        }
+        public void Borrar_Pedido()
+        {
+
+            int Borrado_elemento = 0;
+
+            Console.WriteLine("Cual No. de pedido desea borrar? ");
+            string Element_borrar = Console.ReadLine();
+
+            int.TryParse(Element_borrar, out Borrado_elemento);
+            Queue Recorrer_Cola = new Queue(); // creo nueva cola
+
+            while (miCola.Count > 0)
+            {
+                if ((int)miCola.Peek() == Borrado_elemento) //Peek devuelve un elemento y se converte a entero.  Se compara primer elemento con 
+                {
+                    miCola.Dequeue();// elemento que se eliminar
+                }
+                else
+                {
+                    Recorrer_Cola.Enqueue((int)miCola.Dequeue()); //elemento que se enconlan en la nueva cola cuando no es la opcion seleccionada
+                }
+            }
+            //como hacer una obcion de menu volver a elegir otra opcion y q no se termine otra arriba falta el while cuando la opcion es respues si
+            //q sea swicht y cuando eleija laopcion salir se frena q sea while true
+
+        }
+
+        public void Listar_Pedidos()
+        {
+            if (miCola == null)
+            {
+                Console.WriteLine("No hay cola, favor crear cola opción 1 ");
+            }
+
+            else
+            {
+                foreach(var elem in miCola)
+                {
+                    Console.WriteLine("Su lista de pedidos es: {0} ", elem);
+                }
+            }
+                
+        }
+
+        public void listar_Ultimo_Pedido()
+        {
+            if (miCola == null)
+            {
+                Console.WriteLine("No hay cola, favor crear cola opción 1 ");
+            }
+
+            else
+            {
+                miCola.Reverse();
+                int pedido_ulti = miCola.Peek();
+                miCola.Reverse();//vuelven las pocisiones al estado original
+                Console.WriteLine("El ùltimo pedido es {0}", pedido_ulti);
+            }
+                
+        }
+
+        public void borar_Ultimo_Pedido()
+        {
+            if (miCola == null)
+            {
+                Console.WriteLine("No hay cola, favor crear cola opción 1 ");
+            }
+
+            else
+            {
+                miCola.Reverse();
+                int ultimo_pedido = miCola.Dequeue();// saca el ultimo pedido
+                miCola.Reverse();//vuelven las pocisiones al estado original
+                Console.WriteLine("Se removio  ultimo pedido de la lista");
+            }
+                
+        }
+
+        public void listar_Primer_Pedido()
+
+        {
+            if (miCola == null)
+            {
+                Console.WriteLine("No hay cola, favor crear cola opción 1 ");
+            }
+
+            else
+            {
+                int primer_pedido = miCola.Peek();//Muestra el primer elemento sin eliminarlo
+                Console.WriteLine("El {0}, es el primer pedido de la lista", primer_pedido);
+            }
+                
+        }
+
+        public void Cantidad_pedidos()
+        {
+            if (miCola == null)
+            {
+                Console.WriteLine("No hay cola o no a ingresado pedidos ");
+            }
+
+            else
+            {
+                int cantid_pedid = miCola.Count();
+                Console.WriteLine("El total de pedidos es de {0}", cantid_pedid);
+            }
+                
+        }
+
+        public void Buscar_pedido()
+        {
+            if (miCola == null)
+            {
+                Console.WriteLine("No hay cola o no a ingresado pedidos ");
+            }
+
+            else
+            {
+                Console.WriteLine("Cual pedido desea buscar? ");
+                string buscar_pedido = Console.ReadLine();
+                int buscar = 0;
+                int.TryParse(buscar_pedido, out buscar); // lo convierto en numero
+                int numers = 0; // contador
+
+
+                if (miCola.Contains(buscar)) // Determina si el elemento se encuentra dentro de la cola
+                {
+                    foreach (var item in miCola) //foreach recorre por cada elemento que contiene la cola
+                    {
+                        if (item == buscar)
+                        {
+                            Console.WriteLine("El pedido se encuentra en la pocisión {0}", numers);
+                        }
+                        numers++;
+                    }
+                    Console.WriteLine();
+                }
+
+                else
+                {
+                    Console.WriteLine("El pedido no se encuentra en la lista");
+                }
+            }
+                
+        }
+
+    }
+
+}
---
+++ TrabajoFinal2Estructura_Datos/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TrabajoFinal2Estructura_Datos
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Principal metodos = new Principal(); //instancia de clase de las funciones principales
+            int opcion_Nro = 0;
+            while (opcion_Nro != 11)
+            {
+                string opcionUsuario;
+                //Console.Clear();// Limpia pantalla
+                
+                Console.WriteLine("Bienvenido a TU CombO, que deseas hacer? ");
+                Console.WriteLine("Menu Cliente: " +
+                    "\n1) Crear Cola" +
+                    "\n2) Eliminar Compra Cola " +
+                    "\n3) Agregar Pedido " +
+                    "\n4) Borrar Pedido" +
+                    "\n5) Listar todos los pedidos" +
+                    "\n6) Listar último Pedido" +
+                    "\n7) Listar primer Pedido " +
+                    "\n8) Cantidad de Pedido " +
+                    "\n9) Buscar Pedido" +
+                    "\n10) Borrar ultimo Pedido " +
+                    "\n11) Salir");
+
+                Console.WriteLine("\nIngrese una opción:");
+                opcionUsuario = Console.ReadLine();
+                bool opop  = int.TryParse(opcionUsuario, out opcion_Nro);// Intenta convertirlo sino puede lo deja al valor inicial 
+                Console.WriteLine("\n \n");
+
+                switch (opcion_Nro)
+                {
+                    case 1:
+                        metodos.Crear_Cola();
+                        break;
+
+                    case 2:
+                        metodos.Borrar_Cola();
+                        break;
+
+                    case 3:
+                        metodos.Agregar_Pedido();
+                        break;
+
+                    case 4:
+                        metodos.Borrar_Pedido();
+                        break;
+
+                    case 5:
+                        metodos.Listar_Pedidos();
+                        break;
+
+                    case 6:
+                        metodos.listar_Ultimo_Pedido();
+                        break;
+
+                    case 7:
+                        metodos.listar_Primer_Pedido();
+                        break;
+
+                    case 8:
+                        metodos.Cantidad_pedidos();
+                        break;
+
+                    case 9:
+                        metodos.Buscar_pedido();
+                        break;
+                   
+                    case 10:
+                        metodos.borar_Ultimo_Pedido();
+                        break;
+
+                    case 11:
+                        break;
+
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\n\n Codigo invalido, volvé a intentarlo");
+                        Thread.Sleep(2800);
+                        Console.ResetColor();
+                        break;
+                }
+            }
+
+
+
+        }
+        //no cerrar aplicacion
+
+
+    }
+}
